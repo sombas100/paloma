ee4029b2edc478da18789813000ae6630d9354ff
corey clarke <sombas100@yahoo.com>
2024-07-24 18:34:54 +0100

    changes

--- client/src/Components/Auth/OAuth.tsx
+++ client/src/Components/Auth/OAuth.tsx
@@ -10,6 +10,7 @@ import { useNavigate } from "react-router-dom";
 const OAuth: React.FC = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
+  const apiUrl = import.meta.env.VITE_APP_BASE_URL;
 
   const handleGoogleClick = async () => {
     const auth = getAuth(app);
@@ -18,7 +19,7 @@ const OAuth: React.FC = () => {
 
     try {
       const resultsFromGoogle = await signInWithPopup(auth, provider);
-      const res = await fetch("http://localhost:3000/api/users/google", {
+      const res = await fetch(apiUrl, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
