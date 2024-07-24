d942576b44180070972a86229bdb3465e3c510aa
corey clarke <sombas100@yahoo.com>
2024-07-24 17:13:57 +0100

    Changes to porduction build

--- client/src/Config/axiosInstance.ts
+++ client/src/Config/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_APP_BASE_URL || 'https://paloma-vo48.onrender.com',
+    baseURL: import.meta.env.VITE_APP_BASE_URL || 'https://paloma-vo48.onrender.com/api',
     headers: {
         'Content-Type': 'application/json',
     },
